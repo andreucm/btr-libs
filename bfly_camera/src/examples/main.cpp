46f9981d9801058541b463a47f867366ab05c306
andreu <andreu@beta-robots.com>
2013-09-25 12:21:48 +0200

    Getting num of images to acquire as main argument (user entry)

--- bfly_camera/src/examples/main.cpp
+++ bfly_camera/src/examples/main.cpp
@@ -1,33 +1,52 @@
 
+#include <sstream>
 #include "bflyCamera.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+//constants
+const int numUserEntries = 2;
+//const unsigned int numImagesDefault = 1000;
+
 //this will be user entries
-const unsigned int numImages = 1000;
 const unsigned int imageW = 320;
 const unsigned int imageH = 182;
 const unsigned int pxFormat = RGB8;
 
 //main loop 
 int main(int argc, char *argv[])
-{
+{      
+      //check user entries
+      if ( argc != numUserEntries )
+      {
+            std::cout  << "EXIT due to missing or too many parameters" << std::endl
+                  << "Execute with \"bflyAcquisition [numImages]. \" " << std::endl;
+            return -1;
+      }
+      
       CbflyCamera camera;
       cv::Mat img;
-      unsigned int ii;
-      
+      unsigned int ii, numImages;
+      int retV = 0; 
+            
       //set Window
       cv::namedWindow("rawImage", CV_WINDOW_AUTOSIZE);
       cv::moveWindow("rawImage",20,50);
       
-      //opens camera and starts acquisition loop
       if( camera.isInitOk() )
-      {
-            camera.open();
-            camera.configure(imageW, imageH, pxFormat);
+      {      
+            //get user entries
+            numImages = atoi(argv[1]);
+            std::cout << "Num Images to acquire: " << numImages << std::endl;
+       
+            //opens & configures camera
+            retV += camera.open();
+            retV += camera.configure(imageW, imageH, pxFormat);
             camera.printCameraInfo();
-            camera.startAcquisition();
+            retV += camera.startAcquisition();
+            if (retV != 3*BFLY_SUCCESS) return -1;
             
+            //acquisition loop            
             for (ii=0; ii<numImages; ii++)
             {
                   //prints useful information, each 10 frames
